9690e25ff4b91f4b77b092fc9de0b2003fb1c275
Rafael Gibam <rafael.w.silva@hotmail.com>
2017-10-02 20:12:48 -0300

    TelaExcluirGuia Concluida com sucesso!

--- src/turismoso/Main.java
+++ src/turismoso/Main.java
@@ -1,12 +1,14 @@
 package turismoso;
 
 import TelasCRUD.TelaEditGuia;
+import TelasCRUD.TelaEditLugar;
+import TelasCRUD.TelaExcluirGuia;
 
 
 public class Main {
 
 	public static void main(String[] args) {
-		new TelaEditGuia();
+		new TelaEditLugar();
 
 	}
 
